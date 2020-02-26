47bd5041dbb0545a8aa700744ad70f78c46422c5
gagangill5 <59288784+gagangill5@users.noreply.github.com>
2020-02-26 14:45:42 +0800

    added image object

---
+++ src/Components/Restaurant.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../Styles/restaurant.css";
+
+class Restaurant extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="restaurant"></div>
+      </>
+    );
+  }
+}
+
+export default Restaurant;
