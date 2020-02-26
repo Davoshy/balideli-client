12848a8d2e4089f3859901d8462bf27713381fb2
Vinzenz F.X. Johow <52751209+vfxjohow@users.noreply.github.com>
2020-02-26 15:28:34 +0800

    Added API call to get the categories

--- src/Components/Restaurants.js
+++ src/Components/Restaurants.js
@@ -11,10 +11,21 @@ import "../Styles/restaurants.css";
 class Restaurants extends React.Component {
   // Api Request
   componentWillMount() {
+    // Getting all restaurants for the thumpnails
     Axios.get(`${process.env.REACT_APP_API}/restaurants`)
       .then(res => {
         this.setState({
           restaurants: res.data
+        });
+      })
+      .catch(err => {
+        console.log({ err });
+      });
+    // Getting all categories for the Nav
+    Axios.get(`${process.env.REACT_APP_API}/categories`)
+      .then(res => {
+        this.setState({
+          categories: res.data
         });
       })
       .catch(err => {
